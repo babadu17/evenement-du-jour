8e717e3cc57ce4d3c4f6a3212b40d613fbe0074b
babadu17 <bastianbary17@icloud.com>
2025-09-18 12:55:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 def paragaphe():
-  pass
+  dico = {"18/9":"gfxdnwxstdgfwxd",
+          "19/9":"kuhygfjhdrfgdrsjhyer"
+         }
+  
