f772ec1c9c74bfc427467b857c322dd1a70149b7
babadu17 <bastianbary17@icloud.com>
2025-09-20 18:25:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     today = datetime.today().strftime("%m-%d")
-    
-    # Charger le fichier JSON
     with open("events.json", "r", encoding="utf-8") as f:
         events = json.load(f)
-    
-    # Récupérer la liste d'événements du jour
-    todays_events = events.get(today, ["Aucun événement trouvé pour aujourd'hui."])
-    
+    todays_events = events.get(today, [])
     return render_template("index.html", events=todays_events)
