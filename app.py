4cf488bab54c7df71d30e94c36e7be0ace9ecb80
babadu17 <bastianbary17@icloud.com>
2025-10-05 09:56:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ import os
 import psycopg2
 
 app = Flask(__name__)
-app.secret_key = "une_cle_secrete"  # Obligatoire pour flash
+DB_URL = "postgresql://avis_5iyd_user:mFFNunuA1B0ymaJ60VlhtiFLdjEYhatZ@dpg-d3gjlhe3jp1c73er6ptg-a/avis_5iyd"  # Obligatoire pour flash
 
+def get_connection():
+    return psycopg2.connect(DB_URL)
+    
 @app.route("/")
 def home():
     # Date du jour (jour et mois)
@@ -22,27 +25,32 @@ def home():
 
 @app.route("/enregistrer_avis", methods=["POST"])
 def enregistrer_avis():
-    avis = request.form.get("star")       # texte de l'avis
-    note = request.form.get("avis")       # nombre d'étoiles
+    avis = request.form.get("star")
+    note = request.form.get("avis")
 
     if avis or note:
         contenu = f"{note} étoiles : {avis}"
 
-        conn = psycopg2.connect("postgresql://avis_5iyd_user:mFFNunuA1B0ymaJ60VlhtiFLdjEYhatZ@dpg-d3gjlhe3jp1c73er6ptg-a/avis_5iyd")
-        conn.execute("INSERT INTO avis (texte) VALUES (?)", (contenu,))
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO avis (texte) VALUES (%s)", (contenu,))
         conn.commit()
         conn.close()
-        
+
+        flash("✅ Votre avis a bien été enregistré !", "success")
+
     return redirect("/")
 
 @app.route("/avis")
 def afficher_avis():
-    conn = psycopg2.connect("postgresql://avis_5iyd_user:mFFNunuA1B0ymaJ60VlhtiFLdjEYhatZ@dpg-d3gjlhe3jp1c73er6ptg-a/avis_5iyd")
+    conn = get_connection()
     cur = conn.cursor()
     cur.execute("SELECT texte FROM avis ORDER BY id DESC")
     data = cur.fetchall()
     conn.close()
+    
     return render_template("avis.html", avis_list=data)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
